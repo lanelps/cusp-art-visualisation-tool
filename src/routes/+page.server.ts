797cd1f9f828a23b9453e3630298456fb1a95596
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-04-18 12:28:13 +1000

    Update weather data import in +page.server.ts and video source in +page.svelte

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,7 @@
 // import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-import { fetchWeatherData } from '$lib/metservice';
+import { fetchWeatherData } from '$lib/weather/metservice';
 
 export const load: PageServerLoad = async () => {
 	const data = await fetchWeatherData();
