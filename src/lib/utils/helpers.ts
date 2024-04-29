7d660e8631a8f9fb4d4ba73a2f5da82348353f21
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-04-29 17:02:58 +1000

    Add suncalc library for calculating sunrise and sunset times

--- src/lib/utils/helpers.ts
+++ src/lib/utils/helpers.ts
@@ -1,18 +1,26 @@
 import moment from 'moment-timezone';
+import SunCalc from 'suncalc';
 
 export const getTimeOfDay = () => {
-	const currentDate = moment().tz('Pacific/Auckland');
-	const currentHour = currentDate.hour();
-	const currentMinute = currentDate.minute();
+	const lyttleton = { lat: -43.6030053219916, lon: 172.71710540236882 };
+	const timezone = 'Pacific/Auckland';
 
-	if (currentHour >= 6 && currentHour < 17) {
-		// if between 6am and 5pm, it's day
+	// Get the current date and time in the specified timezone
+	const currentDate = moment().tz(timezone);
+	const currentDateTime = currentDate.toDate();
+
+	// Calculate sunrise and sunset times for the current day
+	const sunTimes = SunCalc.getTimes(currentDateTime, lyttleton.lat, lyttleton.lon);
+	const sunrise = moment(sunTimes.sunrise).tz(timezone);
+	const sunset = moment(sunTimes.sunset).tz(timezone);
+	const nauticalDusk = moment(sunTimes.nauticalDusk).tz(timezone);
+
+	// Compare current time with sunrise and sunset times
+	if (currentDate.isAfter(sunrise) && currentDate.isBefore(sunset)) {
 		return 'day';
-	} else if (currentHour === 17 && currentMinute >= 0 && currentMinute <= 30) {
-		// if between 5pm and 5:30pm, it's sunset
+	} else if (currentDate.isAfter(sunset) && currentDate.isBefore(nauticalDusk)) {
 		return 'sunset';
 	} else {
-		// otherwise, it's night
 		return 'night';
 	}
 };
