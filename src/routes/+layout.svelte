133ca567af8fcc74100447784d21ad18d942af59
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-05-10 11:16:55 +1000

    feat: Add Google Analytics tracking to page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,30 @@
 <script>
 	import '../app.css';
+	import { page } from '$app/stores';
+
+	$: {
+		if (typeof gtag !== 'undefined') {
+			gtag('config', 'G-NMZ2D9S60F', {
+				page_title: document.title,
+				page_path: $page.url.pathname
+			});
+		}
+	}
 </script>
+
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-NMZ2D9S60F">
+	</script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+		gtag('config', 'G-NMZ2D9S60F');
+	</script>
+</svelte:head>
 
 <slot />
