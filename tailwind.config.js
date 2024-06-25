38d38bfe03d662478a21345e1b2bd7b8a889c369
Lane Le Prevost-Smith <lanelps@gmail.com>
2024-06-25 10:23:01 +1000

    feat: Add Tailwind CSS utility functions for breakpoints, grid, and spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+import { breakpoints, createTailWindGrid, spacing } from './src/lib/utils/tailwind';
+
+const { gridSpan, gridColumns } = createTailWindGrid();
+
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		screens: breakpoints,
+		gridColumn: gridSpan,
+		gridColumnStart: gridColumns,
+		gridColumnEnd: gridColumns,
+		spacing: spacing(),
 		extend: {}
 	},
 	plugins: []
